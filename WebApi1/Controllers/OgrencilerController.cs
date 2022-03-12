2a907a04135fdc9cc49cf42a605b2afdb63aea39
BTAkademimcsd <53770975+BTAkademimcsd@users.noreply.github.com>
2022-03-12 13:17:24 +0300

    Add project files.

---
+++ WebApi1/Controllers/OgrencilerController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi1.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    // http://localhost:portno/ogrenciler/actionName
+    [ApiController]
+    public class OgrencilerController : ControllerBase
+    {
+        List<string> ogrenciler = new List<string>() { "BAHAR", "SEMİHA", "SUNA", "ÖNDER", "SERHAT", "FURKAN" };
+
+        public IActionResult Get1()
+        {
+            return Ok(ogrenciler); // HttpStatusCode 200 anlamına gelir...
+        }
+
+        public IActionResult List()
+        {
+            return NotFound("Üzgünüz veriyi bulamadık...."); // httpstatuscode 404 anlamanına gelir..
+        }
+    }
+}
---
+++ WebApi1/Controllers/ValuesController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi1.Controllers
+{
+    // Route => api/controller => şeklinde çağrılabilir...
+    // http://localhost:portNo/api/values şeklinde öğrenci listesine erişilebilir...
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET => veri okumak
+        // POST => veri eklemek
+        // PUT => veri güncellemek
+        // DELETE => veri silmek
+
+        // VERİ
+        List<string> ogrenciler = new List<string>() { "BAHAR", "SEMİHA", "SUNA", "ÖNDER", "SERHAT", "FURKAN" };
+
+        // URI
+        //[HttpGet] // default http request type her zaman gettir...
+        public IActionResult Get()
+        {
+            return Ok(ogrenciler);
+            // işlemler yapıldı
+        }
+
+        // Sadece post requestlerde erişilebilir
+        [HttpPost]
+        public IActionResult Post()
+        {
+            return Ok(true);
+        }
+    }
+}
